d3e8f8475be6c235e88c7b4d17f82a5878179a2a
Ostap Hedz <lordanvil18@gmail.com>
2024-08-07 10:19:31 +0200

    make build

---
+++ index.d898d129.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){event.preventDefault();var r=t.target.closest("a");r&&(e.src=r.href)});
+//# sourceMappingURL=index.d898d129.js.map
